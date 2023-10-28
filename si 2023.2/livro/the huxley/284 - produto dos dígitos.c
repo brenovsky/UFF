73a5ca28f1e1d03ea78b7801a5abc189dac57281
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2023-10-28 18:22:52 -0300

    fix: fixing 284

--- si 2023.2/livro/the huxley/284 - produto dos dígitos.c
+++ si 2023.2/livro/the huxley/284 - produto dos dígitos.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int minor_product()
-{
-
-}
-
 int main()
 {
-    long int n;
+    long int N;
 
-    scanf("%ld", &n);
+    scanf("%ld", &N);
+
+    long int Q = 0;
+    int fator = 9;
+    int contador = 1;
+
+    while (N > 1 && fator > 1) {
+        if (N % fator == 0) {
+            Q += fator * contador;
+            N = N / fator;
+            contador *= 10;
+        }
+        else {
+            fator -= 1;
+        }
+    }
+
+    printf("%ld", Q);
 
     return 0;
-}//não terminado
+}
