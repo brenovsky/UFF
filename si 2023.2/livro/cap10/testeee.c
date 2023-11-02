8c8698095bb6df45c83ee95f0b1d5c5f7c32a3dd
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2023-11-02 16:24:35 -0300

    add: fazendo merda

---
+++ si 2023.2/livro/cap10/testeee.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int unico(int tam, int lista[tam], int elemento)
+{
+    
+}
+
+int main()
+{
+    int array[100] = {1};
+
+    scanf("%d%d", &array[2], &array[10]);
+
+    for (int i = 0; i < 100; i++) {
+        printf("%d", array[i]);
+    }
+
+    return 0;
+}
