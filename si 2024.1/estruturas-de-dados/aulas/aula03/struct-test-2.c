100a049024818b12cddc80eaf7fb970565b34fb3
Breno de Carvalho Correia <brenodecarvalhocorreia@gmail.com>
2024-03-29 18:35:09 -0300

    feat: 'excluirElemento' function

--- si 2024.1/estruturas-de-dados/aulas/aula03/struct-test-2.c
+++ si 2024.1/estruturas-de-dados/aulas/aula03/struct-test-2.c
@@ -23,28 +23,90 @@ void insereInicio(TLista* lista, int valor) {// inserir elemento no inicio da li
     lista -> prox = novo; //ponteiro do começo aponta para o novo inicio
 }
 
+void insereFim(TLista* lista, int valor) {
+
+    TLista* novo = (TLista*) malloc(sizeof(TLista));
+    TLista* p = lista -> prox; //ponteiro auxiliar
+
+    novo -> info = valor;
+    novo -> prox = NULL;
+
+    if (p -> prox == NULL) {
+        lista -> prox = novo;
+    } else {
+        while (p -> prox != NULL) {
+            p = p -> prox;
+        }
+        p -> prox = novo;
+    }
+
+}
+
 void imprimirLista(TLista* lista) {
 
-    TLista* p = lista -> prox;
+    TLista* p;
 
-    while (p != NULL) {
+    for (p = lista -> prox; p != NULL; p = p -> prox) {
         printf("%d ", p->info);
-
-        p = p -> prox;
     }
+    printf("\n\n");
+}
+
+void excluirElemento(TLista* lista, int valor) {//exclui a primeira ocorrencia
+
+    TLista* p = lista -> prox;
+    TLista* a = lista;
+
+    if (p == NULL) {
+        printf("A lista esta vazia");
+    } else {
+        while (p -> info != valor) {
+            p = p -> prox;
+            a = a -> prox;
+        }
+        a -> prox = p -> prox; //substitui o ponteiro do anterior
+        free(p);
+    }
+    imprimirLista(lista);
 }
 
 int main() {
 
-    int input;
+    int input, valor;
     TLista vetor;
 
     cria_lista(&vetor);
 
     do {
+        printf("1 - Inserir no inicio\n2 - Inserir no fim\n3 - Excluir primeira ocorrencia\n4 - Imprimir lista\n5 - Sair\n\n");
+
         scanf("%d", &input);
-        insereInicio(&vetor, input);
-    } while(input != 0);
+
+        switch(input) {
+
+            case 1:
+                scanf("%d", &valor);
+                insereInicio(&vetor, valor);
+                break;
+            case 2:
+                scanf("%d", &valor);
+                insereFim(&vetor, valor);
+                break;
+            case 3:
+                scanf("%d", &valor);
+                excluirElemento(&vetor, valor);
+                break;
+            case 4:
+                imprimirLista(&vetor);
+                break;
+
+            case 5:
+                printf("Encerrando...\n");
+                break;
+            default:
+                printf("Voce escolheu uma opcao invalida. Tente novamente\n");
+        }
+    } while(input != 5);
 
     imprimirLista(&vetor);
 
